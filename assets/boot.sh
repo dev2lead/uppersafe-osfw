f2eaf582512602e482ddb60466c5db16acfeaef9
root <root@debian70.vm>
2018-07-05 11:44:43 +0000

    Fix .gitignore

--- assets/boot.sh
+++ assets/boot.sh
@@ -5,13 +5,13 @@
 ##
 
 ### BEGIN INIT INFO
-# Provides:		dns
+# Provides:		osfw
 # Required-Start:	$local_fs $network $remote_fs $syslog
 # Required-Stop:	$local_fs $network $remote_fs $syslog
 # Default-Start:	2 3 4 5
 # Default-Stop:		0 1 6
-# Short-Description:	UPPERSAFE
-# Description:		UPPERSAFE
+# Short-Description:	OSFW
+# Description:		UPPERSAFE-OSFW
 ### END INIT INFO
 
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
