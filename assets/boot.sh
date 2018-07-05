119dd532a34596a778504976e34aab1de0328d7b
root <root@debian70.vm>
2018-07-05 11:15:10 +0000

    Add init script to launch on boot

---
+++ assets/boot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+##
+# Nicolas THIBAUT
+# nicolas.thibaut@uppersafe.com
+##
+
+### BEGIN INIT INFO
+# Provides:		dns
+# Required-Start:	$local_fs $network $remote_fs $syslog
+# Required-Stop:	$local_fs $network $remote_fs $syslog
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	UPPERSAFE
+# Description:		UPPERSAFE
+### END INIT INFO
+
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+
+SUCCESS=2
+FAILURE=0
+
+if [ "$1" == "start" ]; then
+    sleep 30
+
+    bash /root/osfw/run.sh
+
+    systemctl restart unbound
+
+    exit "$SUCCESS"
+else
+    exit "$FAILURE"
+fi
